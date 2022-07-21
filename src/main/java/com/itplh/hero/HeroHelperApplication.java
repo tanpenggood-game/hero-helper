b27f8b713faf26b7e32d078e74a82484dae64931
tanpenggood <tanpengswpu@163.com>
2022-07-21 18:10:08 +0800

    initial project

---
+++ src/main/java/com/itplh/hero/HeroHelperApplication.java
@@ -0,0 +1,13 @@
+package com.itplh.hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroHelperApplication.class, args);
+    }
+
+}
