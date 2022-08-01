03529082b2a1bb189fe23d94d786be9871665660
tanpenggood <tanpengswpu@163.com>
2022-08-01 22:24:41 +0800

    feat(HeroRegionUser.java): add region user domain

---
+++ src/main/java/com/itplh/hero/domain/HeroRegionUser.java
@@ -0,0 +1,47 @@
+package com.itplh.hero.domain;
+
+import lombok.Data;
+
+@Data
+public class HeroRegionUser {
+
+    /**
+     * 用户身份标识
+     */
+    private String sid;
+    /**
+     * 登陆账号
+     */
+    private String username;
+    /**
+     * 账号密码
+     */
+    private String password;
+    /**
+     * 网络协议
+     * 默认值为http
+     */
+    private String scheme = "http";
+    /**
+     * 域名
+     */
+    private String domain;
+    /**
+     * 端口
+     */
+    private int port;
+    /**
+     * 游戏分区
+     */
+    private String region;
+    /**
+     * 角色名称
+     */
+    private String roleName;
+    /**
+     * 新手标识
+     * 默认值为false，表示非新手
+     */
+    private boolean novice;
+
+}
