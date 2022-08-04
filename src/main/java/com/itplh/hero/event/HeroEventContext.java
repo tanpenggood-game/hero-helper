7fcac7096ee34f5ed6772e50003508bea627d23e
tanpenggood <tanpengswpu@163.com>
2022-08-04 20:13:25 +0800

    feat: event context add resource snapshot & status

--- src/main/java/com/itplh/hero/event/HeroEventContext.java
+++ src/main/java/com/itplh/hero/event/HeroEventContext.java
@@ -1,12 +1,20 @@
 package com.itplh.hero.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.itplh.hero.constant.EventStatusEnum;
+import com.itplh.hero.constant.ParameterEnum;
+import com.itplh.hero.domain.OperationResourceSnapshot;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Data
+@Accessors(chain = true)
 public class HeroEventContext {
 
     private String sid;
@@ -27,13 +35,29 @@ public class HeroEventContext {
      */
     private long actualRunRound;
     /**
+     * 事件状态
+     * <p>
+     * event status reference {@link EventStatusEnum}
+     */
+    private EventStatusEnum status;
+    /**
      * 额外的扩展信息
      * <p>
-     * extendInfo includes parameter as follow:
+     * extendInfo parameter includes parameter as follow:
      * 1. mapping, used to specify the operation object
-     * 2. novice, used to identify novices
+     * 2. novice, set value as true, will enable novice protection when operation resource
+     * 3. isSupplyGrain, set value as true, will be supplied grain before operate every resource
+     * or see {@link ParameterEnum}
      */
     private Map<String, String> extendInfo;
+    /**
+     * 操作快照
+     * <p>
+     * main used to event pause and restart
+     * record operate snapshot before pause, and replay operate after restart
+     */
+    @JsonIgnore
+    private Map<String, OperationResourceSnapshot> operationSnapshotMap;
 
     public HeroEventContext(String sid, String eventName, long targetRunRound, Map<String, String> extendInfo) {
         this.sid = sid;
@@ -52,6 +76,11 @@ public class HeroEventContext {
 
     public String buildURI() {
         return buildURI(sid);
+    }
+
+    public Optional<String> queryExtendInfo(ParameterEnum parameter) {
+        String value = extendInfo.get(parameter.getName());
+        return StringUtils.hasText(value) ? Optional.ofNullable(value) : Optional.empty();
     }
 
 }
