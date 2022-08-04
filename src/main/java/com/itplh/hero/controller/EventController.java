3e73202c9e203908985617eccc74ed8078badfe8
tanpenggood <tanpengswpu@163.com>
2022-08-04 20:29:37 +0800

    chore: adapter for other api adjust; simple validate extendInfo parameter

--- src/main/java/com/itplh/hero/controller/EventController.java
+++ src/main/java/com/itplh/hero/controller/EventController.java
@@ -1,20 +1,25 @@
 package com.itplh.hero.controller;
 
+import com.itplh.hero.constant.ParameterEnum;
 import com.itplh.hero.event.HeroEventContext;
+import com.itplh.hero.event.core.NPCFixedEvent;
 import com.itplh.hero.listener.EventBus;
 import com.itplh.hero.properties.HeroRegionUserProperties;
 import com.itplh.hero.util.EventTemplateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
 @Slf4j
 @RestController
+@RequestMapping("/event")
 public class EventController {
 
     @Autowired
@@ -23,12 +28,12 @@ public class EventController {
     @Autowired
     private HeroRegionUserProperties heroRegionUserProperties;
 
-    @PostMapping("/event/trigger")
+    @PostMapping("/trigger")
     public Result eventTrigger(@RequestParam(required = true) String eventName,
                                @RequestParam(required = false) String sid,
                                @RequestParam(required = false, defaultValue = "1") Long targetRunRound,
                                @RequestBody Map<String, String> extendInfo) {
-        Result validate = validate(eventName, sid);
+        Result validate = validate(eventName, sid, extendInfo);
         if (!validate.isSuccess()) {
             return validate;
         }
@@ -43,7 +48,7 @@ public class EventController {
         return Result.ok(String.format("finish trigger [sid=%s] [eventName=%s]  [isSuccess=%s]", sid, eventName, isSuccess));
     }
 
-    @PostMapping("/event/close")
+    @PostMapping("/close")
     public Result eventClose(String sid) {
         sid = setDefaultSidIfAbsent(sid);
 
@@ -53,7 +58,7 @@ public class EventController {
         return Result.ok(String.format("[sid=%s] [isSuccess=%s]", sid, isSuccess));
     }
 
-    @PostMapping("/event/close-all")
+    @PostMapping("/close-all")
     public Result eventCloseAll() {
         log.info("start close all");
         int closeCounter = operateAll("closed", eventBus::close);
@@ -61,7 +66,7 @@ public class EventController {
         return Result.ok(String.format("finish close all [total closed=%s]", closeCounter));
     }
 
-    @PostMapping("/event/pause")
+    @PostMapping("/pause")
     public Result eventPause(String sid) {
         sid = setDefaultSidIfAbsent(sid);
 
@@ -71,7 +76,7 @@ public class EventController {
         return Result.ok(String.format("[sid=%s] [isSuccess=%s]", sid, isSuccess));
     }
 
-    @PostMapping("/event/pause-all")
+    @PostMapping("/pause-all")
     public Result eventPauseAll() {
         log.info("start pause all");
         int pauseCounter = operateAll("paused", eventBus::pause);
@@ -79,31 +84,31 @@ public class EventController {
         return Result.ok(String.format("finish pause all [total paused=%s]", pauseCounter));
     }
 
-    @PostMapping("/event/continue")
+    @PostMapping("/restart")
     public Result eventContinue(String sid) {
         sid = setDefaultSidIfAbsent(sid);
 
-        log.info("start continue [sid={}]", sid);
-        boolean isSuccess = eventBus.continue0(sid);
-        log.info("finish continue [sid={}] [isSuccess={}]", sid, isSuccess);
+        log.info("start restart [sid={}]", sid);
+        boolean isSuccess = eventBus.restart(sid);
+        log.info("finish restart [sid={}] [isSuccess={}]", sid, isSuccess);
         return Result.ok(String.format("[sid=%s] [isSuccess=%s]", sid, isSuccess));
     }
 
-    @PostMapping("/event/continue-all")
+    @PostMapping("/restart-all")
     public Result eventContinueAll() {
-        log.info("start continue all");
-        int continueCounter = operateAll("continued", eventBus::continue0);
-        log.info("finish continue all [total continued={}]", continueCounter);
-        return Result.ok(String.format("finish continue all [total continued=%s]", continueCounter));
+        log.info("start restart all");
+        int restartCounter = operateAll("restarted", eventBus::restart);
+        log.info("finish restart all [total restarted={}]", restartCounter);
+        return Result.ok(String.format("finish restart all [total restarted=%s]", restartCounter));
     }
 
-    @GetMapping("/event/get")
+    @GetMapping("/get")
     public Result eventOne(String sid) {
         sid = setDefaultSidIfAbsent(sid);
         return Result.ok(eventBus.getEvent(sid));
     }
 
-    @GetMapping("/event/get-all")
+    @GetMapping("/get-all")
     public Result eventAll() {
         return Result.ok(eventBus.getAllEvent());
     }
@@ -121,7 +126,9 @@ public class EventController {
         return sid;
     }
 
-    private Result validate(String eventName, String sid) {
+    private Result validate(String eventName,
+                            String sid,
+                            Map<String, String> extendInfo) {
         if (StringUtils.hasText(sid)) {
             if (!heroRegionUserProperties.getRegionUsers().keySet().contains(sid)) {
                 return Result.error("sid 参数异常");
@@ -133,19 +140,26 @@ public class EventController {
         if (!EventTemplateUtil.hasEventInstance(eventName)) {
             return Result.error("eventName 参数异常");
         }
+        if (Objects.equals(eventName, NPCFixedEvent.class.getSimpleName())) {
+            String resource = ParameterEnum.RESOURCE.getName();
+            if (CollectionUtils.isEmpty(extendInfo) || !extendInfo.containsKey(resource)) {
+                return Result.error(String.format("%s must set %s", NPCFixedEvent.class.getSimpleName(), resource));
+            }
+        }
         return Result.ok();
     }
 
     private int operateAll(String operateLog, Function<String, Boolean> function) {
         int[] successCounter = {0};
-        eventBus.getAllEvent().forEach((k, v) ->
-                v.keySet().forEach(sid -> {
+        eventBus.getAllEvent().stream()
+                .map(event -> event.eventContext().getSid())
+                .forEach(sid -> {
                     boolean isSuccess = function.apply(sid);
                     if (isSuccess) {
                         ++successCounter[0];
                         log.info(operateLog + " [sid={}]", sid);
                     }
-                }));
+                });
         return successCounter[0];
     }
 
