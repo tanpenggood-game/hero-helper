0c62516d89fe1dbc57f006438df51e2ecd6ed97b
tanpenggood <tanpengswpu@163.com>
2022-08-06 19:38:30 +0800

    feat(AutoRefreshSchedule.java): auto refresh each 10 minutes, keep always online

---
+++ src/main/java/com/itplh/hero/schedule/AutoRefreshSchedule.java
@@ -0,0 +1,27 @@
+package com.itplh.hero.schedule;
+
+import com.itplh.hero.context.HeroRegionUserContext;
+import com.itplh.hero.event.core.OnlyRefreshEvent;
+import com.itplh.hero.listener.EventBus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class AutoRefreshSchedule {
+
+    @Autowired
+    private EventBus eventBus;
+
+    @Scheduled(cron = "0 0/10 * * * ? ")
+    public void refresh() {
+        HeroRegionUserContext.getAll().forEach(user -> {
+            if (!eventBus.isRunningEvent(user.getSid())) {
+                eventBus.publishEvent(OnlyRefreshEvent.refreshOnce(user.getSid()));
+            }
+        });
+    }
+
+}
