f97936b0aea6b876497c19f7a63bacd6b81b9934
tanpenggood <tanpengswpu@163.com>
2022-08-10 02:31:58 +0800

    feat(GlobalExceptionHandler.java): add global exception handler

---
+++ src/main/java/com/itplh/hero/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.itplh.hero.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Throwable.class)
+    public Result exceptionHandler(Throwable e) {
+        log.error(e.getMessage(), e);
+        return Result.error(e.getMessage());
+    }
+    
+}
