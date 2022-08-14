12b43cc18f8f1c5b0349b1e2ad011e311e6b279c
tanpenggood <tanpengswpu@163.com>
2022-08-15 00:08:25 +0800

    optimize(ElementUtil.java): queryURIByLinkName method add matchAll parameter, true 100% match, false like match

--- src/main/java/com/itplh/hero/util/ElementUtil.java
+++ src/main/java/com/itplh/hero/util/ElementUtil.java
@@ -10,16 +10,32 @@ import java.util.Optional;
 
 public class ElementUtil {
 
-    public static Optional<String> queryURIByLinkName(Document document, Collection<String> linkNames) {
+    public static Optional<String> queryURIByLinkName(Document document, Collection<String> linkNames, boolean matchAll) {
         if (CollectionUtils.isEmpty(linkNames)) {
             return Optional.empty();
         }
         return Optional.ofNullable(document)
                 .map(doc -> doc.getElementsByTag("a"))
                 .flatMap(elements -> elements.stream()
-                        .filter(e -> linkNames.contains(e.text()))
+                        .filter(e -> {
+                            // 100% match
+                            if (matchAll) {
+                                return linkNames.contains(e.text());
+                            }
+                            // like match
+                            for (String linkName : linkNames) {
+                                if (e.text().indexOf(linkName) != -1) {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        })
                         .findFirst())
                 .map(e -> e.attr("href"));
+    }
+
+    public static Optional<String> queryURIByLinkName(Document document, Collection<String> linkNames) {
+        return queryURIByLinkName(document, linkNames, false);
     }
 
     public static Optional<String> queryURIByLinkName(Document document, String linkName) {
