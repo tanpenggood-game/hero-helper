7f8252f39c011dfa0ae63051d72d6561ccbd15d4
tanpenggood <tanpengswpu@163.com>
2022-09-03 00:20:02 +0800

    chore(Go1000mUtil.java): return game main page, if target is null

--- src/main/java/com/itplh/hero/util/Go1000mUtil.java
+++ src/main/java/com/itplh/hero/util/Go1000mUtil.java
@@ -1,12 +1,12 @@
 package com.itplh.hero.util;
 
 import com.itplh.hero.constant.WorldMapEnum;
+import com.itplh.hero.event.AbstractEvent;
 import org.jsoup.nodes.Document;
 
 import java.util.Objects;
 import java.util.Optional;
 
-import static com.itplh.hero.util.ElementUtil.queryURIByLinkName;
 import static com.itplh.hero.util.GameUtil.requestReturnGameMainPage;
 import static com.itplh.hero.util.MoveUtil.moveAndContinuous;
 import static com.itplh.hero.util.RequestUtil.requestByLinkName;
@@ -18,32 +18,37 @@ public class Go1000mUtil {
      * 神行千里
      * public
      *
-     * @param uri
+     * @param event
      * @param target
      * @param operateLog
      * @return
      */
-    public static Optional<Document> go1000m(String uri, WorldMapEnum target, String operateLog) {
-        return Optional.ofNullable(sleepThenGETRequest(uri, operateLog))
-                .flatMap(document -> go1000m(document, target));
+    public static Optional<Document> go1000m(String uri,
+                                             WorldMapEnum target,
+                                             AbstractEvent event,
+                                             String operateLog) {
+        return Optional.ofNullable(sleepThenGETRequest(uri, event, operateLog))
+                .flatMap(document -> go1000m(event, target));
     }
 
     /**
      * 神行千里
+     * <p>
+     * return game main page, if {@param target} is null
      *
-     * @param document
+     * @param event
      * @param target
      * @return
      */
-    public static Optional<Document> go1000m(Document document, WorldMapEnum target) {
+    public static Optional<Document> go1000m(AbstractEvent event, WorldMapEnum target) {
+        // return game main page
+        Document document = requestReturnGameMainPage(event).orElse(null);
         if (Objects.isNull(target)) {
             return Optional.ofNullable(document);
         }
-        // return game main page
-        document = requestReturnGameMainPage(document).orElse(null);
         // check is extend
         if (target.isExtend()) {
-            return go1000mExtend(document, target);
+            return go1000mExtend(event, target);
         }
         // current page is target page
         boolean isTargetPage = isTargetPage(document, target);
@@ -52,27 +57,28 @@ public class Go1000mUtil {
         }
         return Optional.ofNullable(document)
                 // request-功能菜单
-                .flatMap(doc -> requestByLinkName(doc, "功能菜单", "robot request-功能菜单"))
+                .flatMap(doc -> requestByLinkName(event, "功能菜单", "robot request-功能菜单"))
                 // request-神行千里
-                .flatMap(doc -> requestByLinkName(doc, "神行千里", "robot request-神行千里"))
-                .flatMap(doc -> requestByLinkName(doc, target.getName(), "robot 神行千里-" + target.getName()));
+                .flatMap(doc -> requestByLinkName(event, "神行千里", "robot request-神行千里"))
+                .flatMap(doc -> requestByLinkName(event, target.getName(), "robot 神行千里-" + target.getName()));
     }
 
     /**
      * 神行千里
      * 扩展的传送地图点
      *
-     * @param document
+     * @param event
      * @param target
      * @return
      */
-    private static Optional<Document> go1000mExtend(Document document, WorldMapEnum target) {
+    private static Optional<Document> go1000mExtend(AbstractEvent event, WorldMapEnum target) {
+        Document document = event.eventContext().currentDocument();
         // current page is target page
         if (isTargetPage(document, target)) {
             return Optional.ofNullable(document);
         }
-        return go1000m(document, target.getTransferPoint())
-                .flatMap(doc -> moveAndContinuous(doc, target.getRouters()));
+        return go1000m(event, target.getTransferPoint())
+                .flatMap(doc -> moveAndContinuous(event, target.getRouters()));
     }
 
     /**
@@ -83,7 +89,7 @@ public class Go1000mUtil {
      * @return
      */
     private static boolean isTargetPage(Document document, WorldMapEnum target) {
-        return queryURIByLinkName(document, target.getTargetPageUniqueLinkName()).isPresent();
+        return ElementUtil.queryURILikeLinkName(document, target.getTargetPageUniqueLinkName()).isPresent();
     }
 
 }
