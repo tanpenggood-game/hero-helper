77bcf4eadc8505ec11260102b7802794a71ae2df
tanpenggood <tanpengswpu@163.com>
2022-09-03 00:12:54 +0800

    optimize(MoveUtil.java): return document after move, if direction is exists; otherwise return original document.

--- src/main/java/com/itplh/hero/util/MoveUtil.java
+++ src/main/java/com/itplh/hero/util/MoveUtil.java
@@ -1,6 +1,9 @@
 package com.itplh.hero.util;
 
 import com.itplh.hero.constant.DirectionEnum;
+import com.itplh.hero.event.AbstractEvent;
+import com.itplh.hero.event.HeroEventContext;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 
 import java.util.List;
@@ -8,30 +11,42 @@ import java.util.Optional;
 
 import static com.itplh.hero.util.RequestUtil.sleepThenGETRequest;
 
+@Slf4j
 public class MoveUtil {
 
     /**
      * 向某个方向移动一步
      *
-     * @param document
+     * @param event
      * @param direction
-     * @return
+     * @return document after move, if direction is exists; otherwise return original document.
      */
-    public static Optional<Document> move(Document document, DirectionEnum direction) {
-        return ElementUtil.queryURIByAccesskey(document, direction.getAccessValue())
-                .map(uri -> sleepThenGETRequest(uri, "robot move-" + direction.getLinkName()));
+    public static Optional<Document> move(AbstractEvent event, DirectionEnum direction) {
+        Document document = event.eventContext().currentDocument();
+        // move, if direction is exists
+        Optional<String> directionURIOptional = ElementUtil.queryURIByAccesskey(document, direction.getAccessValue());
+        if (directionURIOptional.isPresent()) {
+            document = sleepThenGETRequest(directionURIOptional.get(), event, "robot move-" + direction.getLinkName());
+        } else {
+            HeroEventContext heroEventContext = event.eventContext();
+            log.debug("direction isn't exists [sid={}] [eventName={}] [operateName={}] [directionName={}]",
+                    heroEventContext.getUser().getSid(), heroEventContext.getEventName(),
+                    heroEventContext.getCurrentOperationResource().getOperateName(), direction.getName());
+        }
+        return Optional.ofNullable(document);
     }
 
     /**
      * 根据路由连续移动
      *
-     * @param document
+     * @param event
      * @param routers
      * @return
      */
-    public static Optional<Document> moveAndContinuous(Document document, List<DirectionEnum> routers) {
+    public static Optional<Document> moveAndContinuous(AbstractEvent event, List<DirectionEnum> routers) {
+        Document document = null;
         for (DirectionEnum router : routers) {
-            document = move(document, router).orElse(null);
+            document = move(event, router).orElse(null);
         }
         return Optional.ofNullable(document);
     }
